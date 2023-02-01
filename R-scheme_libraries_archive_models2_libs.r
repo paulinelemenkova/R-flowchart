49f42038bf8fcf65e35af606a74d18c35b5fef3f
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-02-01 17:00:19 +0100

    Add files via upload

---
+++ R-scheme_libraries_archive_models2_libs.r
@@ -0,0 +1,71 @@
+#----------------------------->
+grViz("
+digraph dot {
+
+graph [layout = dot, rankdir = TB, nodesep = 0.10, ranksep = .10,]
+
+node [shape = circle,
+      style = filled,
+      color = grey,
+      label = '']
+
+node [fillcolor = LavenderBlush1]
+    a [label = 'seismostorm', color = coral3]
+
+node [shape = box, group = true, color = darkolivegreen, fixedsize = true, width = 2.0]
+    b [label = 'archive', fillcolor = Thistle1];
+    c [label = 'Models', fillcolor = Powderblue];
+    
+node [fillcolor = Lightblue1, shape = box, color = darkorange3, fixedsize = true, width = 1.5]
+    d [label = '__init__.py'];
+    e [label = 'regressors.py'];
+    f [label = 'scan.py'];
+    g [label = 'segment.py'];
+    h [label = 'tile.py'];
+    i [label = 'types.py'];
+
+node [fillcolor = Lavender, shape = box, color = darkorange3, fixedsize = true, width = 1.5]
+    j [label = 'display_tiles.py'];
+    k [label = 'main_analyse.py'];
+    l [label = 'main_auto.py'];
+    mm [label = 'main_convert.py'];
+    nn [label = 'main_export.py'];
+    oo [label = 'main_timestamp.py'];
+    pp [label = 'main_vectorize.py'];
+    qq [label = 'open_mat.py'];
+    rr [label = 'read_mseed.py'];
+    ss [label = 'segtest.py'];
+    tt [label = 'test_annot.py'];
+
+edge [color = grey, arrowhead = none, arrowtail = none]
+    a -> {b c}
+    b -> j -> k -> l -> mm -> nn -> oo -> pp -> qq -> rr -> ss -> tt
+    c -> d -> e -> f -> g -> h -> i
+
+edge [color = grey, arrowhead = none, arrowtail = none]
+}")
+
+j [label = 'display_tiles.py', fillcolor = Thistle1];
+k [label = 'main_analyse.py', fillcolor = Thistle2];
+l [label = 'main_auto.py', fillcolor = Thistle3];
+mm [label = 'main_convert.py', fillcolor = Plum1];
+nn [label = 'main_export.py', fillcolor = Plum2];
+oo [label = 'main_timestamp.py', fillcolor = Plum3];
+pp [label = 'main_vectorize.py', fillcolor = Orchid1];
+qq [label = 'open_mat.py', fillcolor = Orchid2];
+rr [label = 'read_mseed.py', fillcolor = Orchid3];
+ss [label = 'segtest.py', fillcolor = MediumPurple1];
+tt [label = 'test_annot.py', fillcolor = MediumPurple2];
+
+
+j [label = 'display_tiles.py'];
+k [label = 'main_analyse.py'];
+l [label = 'main_auto.py'];
+mm [label = 'main_convert.py'];
+nn [label = 'main_export.py'];
+oo [label = 'main_timestamp.py'];
+pp [label = 'main_vectorize.py'];
+qq [label = 'open_mat.py'];
+rr [label = 'read_mseed.py'];
+ss [label = 'segtest.py'];
+tt [label = 'test_annot.py'];
