981c686ff6b105ec587a95db03b1dca8c3ad5c21
Polina Lemenkova <pauline.lemenkova@gmail.com>
2024-02-24 17:10:52 +0100

    Add files via upload

--- R-diagram_24022024.r
+++ R-diagram_24022024.r
@@ -11,26 +11,26 @@ digraph Polina_diagram {
        fixedsize = true
        width = 2.0]
   
-  A [label = 'Remote Sensing', fontsize = 22, color = magenta, width=2.5, linewidth = 0.5]
+  A [label = 'Images multispectrales \nLandsat 8-9 OLI/TIRS', fontsize = 22, color = magenta, width=2.5, linewidth = 0.5]
   #
   subgraph cluster0 {
   node [fillcolor = Bisque, shape = egg, fontname = Helvetica, fontcolor = darkslategray, shape = rectangle, fixedsize = true, width = 2.0, color = darkslategray, style = rounded]
-  A1 [label = 'GRASS GIS', shape = rectangle, fontsize = 19, height = 1.0, width = 2.5, style = rounded]
+  A1 [label = 'GRASS SIG', shape = rectangle, fontsize = 19, height = 1.0, width = 2.5, style = rounded]
   }
   #
   # edges
   A -> {A1} [fontcolor = red,
                     color = red, style = dashed]
   
-  E [label = 'Image \npreprocessing', fontcolor = darkgreen, shape = component, fontsize = 18, height = 1.5 ]
+  E [label = 'Prétraitement des \nimages satellites', fontcolor = darkgreen, shape = component, fontsize = 18, height = 1.5 ]
   
   subgraph cluster6 {
   node [fillcolor = Bisque, shape = egg, fontname = Helvetica, fontcolor = darkslategray, shape = rectangle, fixedsize = true, width = 2.5, color = darkslategray]
-  E1 [label = '1. r.import', fontcolor = black, shape = rectangle, width = 3.5, height = 1.3, fontsize = 18]
-  E2 [label = '2. g.region', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  E3 [label = '3. r.list', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  E4 [label = '4. i.landsat.toar', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  E5 [label = '5. r.composite', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  E1 [label = '1. importer les images \nr.import', fontcolor = black, shape = rectangle, width = 3.5, height = 1.3, fontsize = 18]
+  E2 [label = '2. définir étendue \ncartographique \ng.region', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  E3 [label = '3. générer une liste \ndes fichiers \nr.list', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  E4 [label = '4. visualization \nd.mon wx0', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  E5 [label = '5. enregistrement \ndu fichier \nd.out.file', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
   }
   E1 -> E2 [fontcolor = red, color = red, style = dashed]
   E2 -> E3 [fontcolor = red, color = red, style = dashed]
@@ -38,16 +38,14 @@ digraph Polina_diagram {
   E4 -> E5 [fontcolor = red, color = red, style = dashed]
   E -> {E1} [fontcolor = red, color = red, style = dashed]
   
- # E3 -> F [fontcolor = red, color = red, style = dashed]
-  
-  F [label = 'Image \nprocessing', fontcolor = black, height = 1.5, width = 2.5, shape = component, fontsize = 20]
+  F [label = 'Traitement des \ndonnées raster', fontcolor = black, height = 1.5, width = 2.5, shape = component, fontsize = 20]
   subgraph cluster6 {
   node [fillcolor = Bisque, shape = egg, fontname = Helvetica, fontcolor = darkslategray, shape = rectangle, fixedsize = true, width = 2.5, color = darkslategray]
-  F5 [label = '5. Cartographic visualization \n mapping \nd.rast, d.legend modules', fontcolor = black, shape = rectangle, width = 3.5, height = 1.3, fontsize = 18]
-  F4 [label = '4. Accuracy \nassessment', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  F3 [label = '3. Classification\n k-means \ni.maxlik module', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  F2 [label = '2. Clustering \ni.cluster module', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  F1 [label = '1. Grouping \ni.group module', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  F5 [label = '5. cartographie \nd.rast, d.legend', fontcolor = black, shape = rectangle, width = 3.5, height = 1.3, fontsize = 18]
+  F4 [label = '4. compositions \ncolorées \nr.composite', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  F3 [label = '3. copier des bandes \nLandsat \ng.list rast \ng.copy', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  F2 [label = '2. vérifier les \nmétadonnées \nr.info', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  F1 [label = '1. gdalinfo \nr.in.gdal', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
   }
   F1 -> F2 [fontcolor = red, color = red, style = dashed]
   F2 -> F3 [fontcolor = red, color = red, style = dashed]
@@ -56,14 +54,14 @@ digraph Polina_diagram {
   
   F -> {F1 F2 F3 F4 F5} [fontcolor = red, color = red, style = dashed]
   #
-  G [label = 'Machine \nLearning', fontcolor = black, height = 1.5, width = 2.5, shape = component, fontsize = 20]
+  G [label = 'Calcul des indices \nde végétation', fontcolor = black, height = 1.5, width = 2.5, shape = component, fontsize = 20]
   subgraph cluster6 {
   node [fillcolor = Bisque, shape = egg, fontname = Helvetica, fontcolor = darkslategray, shape = rectangle, fixedsize = true, width = 2.5, color = darkslategray]
-  G1 [label = 'r.random', fontcolor = black, shape = rectangle, width = 3.5, height = 1.3, fontsize = 18]
-  G2 [label = 'i.group', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  G3 [label = 'r.learn.train', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  G4 [label = 'r.learn.predict', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
-  G5 [label = 'r.category', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  G1 [label = 'i.landsat.toar', fontcolor = black, shape = rectangle, width = 3.5, height = 1.3, fontsize = 18]
+  G2 [label = 'i.vi: DVI', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  G3 [label = 'i.vi: SAVI', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  G4 [label = 'i.vi: NDVI', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
+  G5 [label = 'i.vi: CI', fontcolor = black, shape = rectangle, width = 2.5, height = 1.3, fontsize = 18]
   }
   G1 -> G2 [fontcolor = red, color = red, style = dashed]
   G2 -> G3 [fontcolor = red, color = red, style = dashed]
